8f376608e491aaf7a11e9193e2a1f389cbcbe316
Ниязбек Аманжолов <47739624+NiyazbekAmanzholov@users.noreply.github.com>
2020-01-05 15:36:07 +0600

    Update create.php

--- imageFile/create.php
+++ imageFile/create.php
@@ -1,14 +1,17 @@
 <?php 
 
+function uploadImage($image)
+{
 
-//var_dump($_FILES['image']);
-
-$name = $_FILES['image']['name'];//название папки
+$name = $image['name'];//название папки
 $tmp_name = $_FILES['image']['tmp_name'];//место где лежит 
 
-//move_uploaded_file($tmp_name, "uploads/image.png");
-
 move_uploaded_file($tmp_name,  "uploads/" . $name);//место куда переносим
+
+}
+
+uploadImage($_FILES['image']);
+
 
  ?>
 
